7dad7ddd853189808d650278c462f1c8e50096ec
kajiyama <dev.kajiyama@gmail.com>
2018-04-25 19:58:16 +0900

    Modify parts reviewer pointed out

--- README.md
+++ README.md
@@ -1,17 +1,42 @@
 # ud_log_analysis
 Source code for Logs Analysis Project
 
-## analyze.py includes three queries for analyze and outputs following results.
+**analyze.py includes three queries and outputs following results.**
 
 1. What are the most popular three articles of all time?
 1. Who are the most popular article authors of all time?
 1. On which days did more than 1% of requests lead to errors?
 
-# Install
+# Install following tools.
+**Virtual Machine**
+1. [vagrant](https://www.vagrantup.com/)
+2. [virtualbox](https://www.virtualbox.org/wiki/Download_Old_Builds_5_1) (ver 5.1)
 
-1. `git clone https://github.com/Kajitaku/ud_log_analysis.git`
-1. `cd ud_log_analysis`
-1. `python analyze.py` to run.
+**Git**
+If you don't already have Git installed, download Git from [git-scm.com](https://git-scm.com/downloads)
+
+# Set up
+**Virtual Machine**
+1. Run `git clone https://github.com/udacity/fullstack-nanodegree-vm.git` to get virtual machine environment.
+2. `cd fullstack-nanodegree-vm`
+3. `vagrant up` to lunch a virtual machine.
+
+**Database**
+1. Get a mock PostgreSQL database zip file from [here](https://d17h27t6h515a5.cloudfront.net/topher/2016/August/57b5f748_newsdata/newsdata.zip)
+2. Unzip the zip and put extracted newsdata.sql into ./fullstack-nanodegree-vm/vagrant/
+3. `vagrant ssh` to login a virtual machine.
+4. Move to /vagrant inside it.
+5. Run `psql -d news -f newsdata.sql` to load database data.
+
+**Source code**
+1. Run `cd ./fullstack-nanodegree-vm/vagrant/`
+2. Run `git clone https://github.com/Kajitaku/ud_log_analysis.git` to get analysis code.
+
+# How to use
+1. `cd ./fullstack-nanodegree-vm/vagrant/`
+2. `vagrant up`
+3. `vagrant ssh`
+4. `cd /vagrant/ud_log_analysis`
+5. `python analyze.py`
 
 # License
-MIT
--- analyze.py
+++ analyze.py
@@ -2,50 +2,100 @@
 
 import psycopg2
 
-DBNAME = "news"
-db = psycopg2.connect(database=DBNAME)
-c = db.cursor()
 
-print "1. What are the most popular three articles of all time?"
+def db_connect():
+    """
+    Creates and returns a connection to the database defined by DBNAME,
+    as well as a cursor for the database.
+    Returns:
+        db, c - a tuple. The first element is a connection to the database.
+                The second element is a cursor for the database.
+    """
+    db = psycopg2.connect(database="news")
+    c = db.cursor()
+    return db, c
 
-query_1 = (
-    "select (select title from articles where slug = "
-    "substr(log.path, 10)) as title, count(*) as pv from log "
-    "where path <> '/' group by title order by pv desc limit 3"
-    )
-c.execute(query_1)
-for row in c:
-    print "\"{}\" - {} views".format(row[0], row[1])
 
-print "\n2. Who are the most popular article authors of all time?"
+def execute_query(query):
+    """
+    execute_query takes an SQL query as a parameter.
+    Executes the query and returns the results as a list of tuples.
+    args:
+    query - an SQL query statement to be executed.
 
-join_2_1 = "inner join articles on substr(log.path, 10) = articles.slug"
-join_2_2 = "inner join authors on articles.author = authors.id"
-query_2 = (
-    "select authors.name, count(*) as views from log "
-    "{} {} "
-    "group by authors.name order by views desc limit 5"
-    ).format(join_2_1, join_2_2)
-c.execute(query_2)
-for row in c:
-    print "{} - {} views".format(row[0], row[1])
+    returns:
+    A list of tuples containing the results of the query.
+    """
+    db, c = db_connect()
+    c.execute(query)
+    return c
+    db.close()
 
-print "\n3. On which days did more than 1% of requests lead to errors?"
 
-subquery_3_1 = (
-    "(select to_char(time, 'Mon DD, YYYY') as Date, count(*) as errors "
-    "from log where status like '4%' group by Date)"
-    )
-subquery_3_2 = (
-    "(select to_char(time, 'Mon DD, YYYY') as Date, count(*) as sum "
-    "from log group by Date)"
-    )
-query_3 = (
-    "select t1.Date, ROUND(t1.errors*100.0/t2.sum, 1) "
-    "from {} as t1 inner join {} as t2 on (t1.Date = t2.Date) "
-    "where ROUND(t1.errors*100.0/t2.sum, 1) >= 1.0"
-    ).format(subquery_3_1, subquery_3_2)
-c.execute(query_3)
-for row in c:
-    print "{} - {}% errors".format(row[0], row[1])
-db.close()
+def print_top_articles():
+    """Prints out the top 3 articles of all time."""
+    print "1. What are the most popular three articles of all time?"
+    query = """
+            SELECT title, count(*) AS pv
+            FROM articles a INNER JOIN log l
+            ON '/article/' || a.slug = l.path
+            GROUP BY title, path
+            ORDER BY pv DESC
+            LIMIT 3;
+            """
+    results = execute_query(query)
+
+    for row in results:
+        print "\"{}\" - {} views".format(row[0], row[1])
+
+
+def print_top_authors():
+    """Prints a list of authors ranked by article views."""
+    print "2. Who are the most popular article authors of all time?"
+    query = """
+            SELECT authors.name, count(*) AS views
+            FROM log l INNER JOIN articles a
+            ON '/article/' || a.slug = l.path
+            INNER JOIN authors
+            ON a.author = authors.id
+            GROUP BY authors.name
+            ORDER BY views DESC
+            LIMIT 5;
+            """
+    results = execute_query(query)
+    for row in results:
+        print "{} - {} views".format(row[0], row[1])
+
+
+def print_errors_over_one():
+    """
+    Prints out the days where more than 1%
+    of logged access requests were errors.
+    """
+    print "3. On which days did more than 1% of requests lead to errors?"
+    query = """
+            SELECT to_char(t1.Date, 'FMMonth DD, YYYY'),
+                 ROUND(t1.errors*100.0/t2.sum, 1)
+            FROM (
+            SELECT time::date AS Date, count(*) AS errors
+            FROM log
+            WHERE status LIKE '4%'
+            GROUP BY Date
+            ) t1
+            INNER JOIN (
+            SELECT time::date AS Date, count(*) AS sum
+            FROM log
+            GROUP BY Date
+            ) t2
+            ON t1.Date = t2.Date
+            WHERE t1.errors*100/t2.sum > 1;
+            """
+    results = execute_query(query)
+
+    for row in results:
+        print "{} - {}% errors".format(row[0], row[1])
+
+if __name__ == '__main__':
+    print_top_articles()
+    print_top_authors()
+    print_errors_over_one()
